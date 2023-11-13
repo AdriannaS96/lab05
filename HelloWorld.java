eca93333e42f89b5dff547579cbfccb5f8bc0f81
Ubuntu <ubuntu@ip-172-31-53-240.ec2.internal>
2023-11-13 13:34:52 +0000

    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,10 +4,8 @@ class HelloWorld {
     // Your program begins with a call to main().
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[]) {
-        System.out.println("Hello, World");
+        System.out.println("Hello World");
         System.out.println("Hello again!");
-        System.out.println("Hello1");
-        System.out.println("Hello3");
     }
 }
 
